3035043cda3087316aeaadb290831520c3c89a04
ntutar <nazmitutar@gmail.com>
2020-11-11 11:54:53 +1000

    added driver class

--- src/test/java/com/vytrack/utils/Driver.java
+++ src/test/java/com/vytrack/utils/Driver.java
@@ -13,8 +13,10 @@ import java.util.concurrent.TimeUnit;
 
 public class Driver {
     private static WebDriver driver;
+
     private Driver() {
     }
+
     public static WebDriver getDriver() {
         if (driver == null) {
             String browser = ConfigurationReader.getProperty("browser");
@@ -48,12 +50,23 @@ public class Driver {
                         e.printStackTrace();
                     }
                     break;
+                case "remote-firefox":
+                    try {
+                        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+                        desiredCapabilities.setBrowserName("firefox");
+                        URL gridUrl = new URL("http://3.82.5.142:4444/wd/hub");
+                        driver = new RemoteWebDriver(gridUrl, desiredCapabilities);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                    break;
                 default:
                     throw new RuntimeException("No such a browser yet!");
             }
         }
         return driver;
     }
+
     public static void closeDriver() {
         if (driver != null) {
             driver.quit();
