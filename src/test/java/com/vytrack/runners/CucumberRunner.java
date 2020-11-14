390f67ea15373d11f3d5c870d748465023ad1618
ntutar <nazmitutar@gmail.com>
2020-11-15 01:10:08 +1000

    added CucumberRunner2 class

--- src/test/java/com/vytrack/runners/CucumberRunner.java
+++ src/test/java/com/vytrack/runners/CucumberRunner.java
@@ -10,17 +10,21 @@ import org.junit.runner.RunWith;
                 "rerun:target/rerun.txt",
                 "json:target/cucumber.json"
         },
-        features = "src/test/resources/features",
+        features = {
+                "src/test/resources/features/activities"
+        },
         glue = "com/vytrack/step_definitions",
         dryRun = false,
-        tags = "@negative_login",
+        tags = "",
         publish = true
 )
 public class CucumberRunner {
         //tags = "@s_o and @with_two_columns",
         // scenario must have BOTH tags
         //java logic if(@s_o && @with_two_columns)
+
         //tags = "@s_o or @with_two_columns",
         // scenario must have EITHER OR tags
         //java logic if(@s_o || @with_two_columns),
 }
+
